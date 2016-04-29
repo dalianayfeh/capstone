30d26110761d3cfb46c30386819e8b26520410c1
Dalia <dnayfeh@uw.edu>
2016-04-29 13:42:15 -0700

    about

---
+++ eVolve/app/src/main/java/com/example/iguest/evolve/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.iguest.evolve;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+//    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        mWebView = (WebView) findViewById(R.id.acitivity_main_webview);
+//        WebSettings webSettings = mWebView.getSettings();
+//        webSettings.setJavaScriptEnabled(true);
+    }
+}
